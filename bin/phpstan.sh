e56f419dfa391dcaf1ab988ace757066c365ea37
paylex-eng <alex.lemmaire@gmail.com>
2024-06-12 14:47:50 +0200

    add all for merge script

--- bin/phpstan.sh
+++ bin/phpstan.sh
@@ -1,10 +1,6 @@
-#!/bin/bash -e
-
-echo "lancement du script phpstan.sh"
-
 APP_NAME=$(basename "$PWD")
-echo "Entrez 1 pour génrer un fichier de log PHPStan"
-echo "Entrez 2 pour afficher le résultat sur les log dans le terminal"
+echo -e "\033[1;34mEnter 1 to generate a PHPStan log file\033[0m"
+echo -e "\033[1;34mEnter 2 to display the result on the terminal logs\033[0m"
 
 read number
 
@@ -12,16 +8,15 @@ case $number in
     1)
         echo ________________________________________________date : $(date '+%Y-%m-%d_%H-%M-%S')__________________________________________________ >> var/log/phpstan/report_phpstan.txt
         docker exec "${APP_NAME}"-php vendor/bin/phpstan analyse src >> var/log/phpstan/report_phpstan.txt
-        echo -e "\033[32mL'analyse PHPStan est terminée. Vérifiez le fichier report_phpstan.txt dans var/log/phpstan pour les résultats.\033[0m"
+        echo -e "\033[32mPHPStan analysis is complete\033[0m"
+        echo -e "\033[33mThe file is saved in var/log/phpstan/report_phpstan.txt\033[0m"
         ;;
     2)
-        echo "Affichage du résultat sur les logs dans le terminal"
+        echo "Displaying the result on the terminal logs"
         docker exec "${APP_NAME}"-php vendor/bin/phpstan analyse src
         ;;
     *)
-        echo "Veuillez entrer 1 ou 2"
+        echo -e "\033[1;31mPlease enter 1 or 2\033[0m"
         exit 1
         ;;
 esac
-echo "fin du script phpstan.sh"
-
